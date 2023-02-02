3ea395465e04fe2c33237beabb935d3fc4185ec5
Alirio Diaz <aliriodi@email.com>
2023-02-02 14:16:31 -0300

    2

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,30 @@
-import './App.css';ls
+import './App.css';
 
 
 function App() {
-     const dato = () => async () => {
-    await fetch("http://localhost:3001/db")
-      .then((response) => {console.log(response.json())})
+  
+     async function dato() {
+      const respuesta = await fetch('http://localhost:3001/db')
+      .then((response) => {response.json()})
       .catch((error) => console.log(error));
-  };
-   dato();
+      const dato2 =  respuesta;
+      console.log(dato2)
+       };
+   const alirio = dato();
   
   return (
     <div className="App">
       <header className="App-header">
       <nav>
-  <a href="#page-1">Bloque1</a>
-  <a href="#page-2">Bloque2</a>
-  <a href="#page-3">Bloque3</a>
+  <a href="#page-1">Block1</a>
+  <a href="#page-2">Block2</a>
+  <a href="#page-3">Block3</a>
 </nav>
 <div className="scroll-container">
   <div className="scroll-page" id="page-1">
+    {alirio && alirio.length? alirio[0].name:null}
     {}
-    {console.log(dato())}
-
+  1
   </div>
   <div className="scroll-page" id="page-2">2</div>
   <div className="scroll-page" id="page-3">3</div>
