2fcc5c408d8959a9b5dcc052051868486d97e920
Alirio Diaz <aliriodi@email.com>
2023-02-12 16:48:05 -0300

    trabajando en el form contacto

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,7 @@ import { useState, useEffect } from "react";
 import './App.css'
 import backI from './back2.png'
 import forwardI from './forward.png'
+import Contacto from "./contacto/Contacto";
 
 function App() {
   //esta declaracion el array es el primero data es la variable que deseamos usar
@@ -60,7 +61,7 @@ const timerNav = async () => {
   <a href="#page-1">Block1</a>
   <a href="#page-2">Block2</a>
   <a href="#page-3">Block3</a>
-  <a href="#page-4">Block4</a>
+  <a href="#page-4"><Contacto /></a>
 </nav>
 
 {/* SECCION 1 */}
